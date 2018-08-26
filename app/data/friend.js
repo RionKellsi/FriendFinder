167fc027eced01de63efc2f8a6324c5aca267d5c
Rion <rionirvin>
2018-08-25 21:24:00 -0500

    Updated

---
+++ app/data/friend.js
@@ -0,0 +1,20 @@
+var friendList = [
+    {
+      name: 'Rion Irvin',
+      profilePic: 'https://scontent.ford4-1.fna.fbcdn.net/v/t1.0-9/16266327_1729711450388349_6902890598876939862_n.jpg?_nc_cat=0&oh=c713eddd25c334fce5089c3c099b7dbc&oe=5C34C373',
+      scores: [
+        "4",
+        "4",
+        "5",
+        "3",
+        "3",
+        "5",
+        "5",
+        "3",
+        "3",
+        "5"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
